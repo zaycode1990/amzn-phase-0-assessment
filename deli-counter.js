7ac62a956466503614e3a94280672c3403910522
Isaiah <ijandicos90@gmail.com>
2022-04-05 18:00:02 -0400

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,24 +1,62 @@
-// 1. Write your functions here
+const katzDeli = []
+
+function takeANumber(katzQue, customerName) {
+    katzQue = katzQue.push(customerName)
+    for (let [index, customer] of katzDeli.entries()) {
+        if (customer === customerName)
+            console.log(`Welcome, ${customer}. You are number ${index += 1} in line.`)
+    }
+
+   
+}
+
+/*used a for of loop with entries command so I could easily 
+have access to the index and value of the passed in array in global scope
+it also allowed me to strictly pass in the customerName as a variable so that only one string sentence would 
+get logged out*/
+
+let lineQue = []
 
 
-// 2. Example Usage
+function line(katzQue) {
+    for (let i = 0; i < katzQue.length; i++) {
+        let customerEntry = katzQue[i];
+        lineQue.push(' ' + [i + 1] + '. ' + customerEntry)
+      /* pushed the string sentence to an array based on the array index and values passed in. 
+      the for loop anchors the variables to the passed in array so that it always updates the string sentence*/
+    }
 
-// const katzDeli = []
+if (katzQue.length === 0) {
+    return "The line is currently empty."
+}
+else
+lineUpdate = lineQue.splice(0,3)
+// had to add a splice to get the lineQue under control and to also modify it destructively 
+    return "\"The line is currently:" + `${lineUpdate.join("")}"`
+// added a join command so that the formating would be more accurate and lineUpdate coulde become a more pure string
+}
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-// nowServing(katzDeli) //=> "Currently serving Ada."
+function nowServing(katzQue) {
+    if (katzQue !== []) {
+        console.log(`"Currently serving ${katzQue.shift()}"`)
+              /* i recently read that shift can be used to call a value in an array 
+              and simulatanously remove it so I utilized shift
+              */
+    }
+    else
+        return "There is nobody waiting to be served"
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+}
 
-// takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
-
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
-
-// nowServing(katzDeli) //=> "Currently serving Grace."
-
-// line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+console.log(line(katzDeli))
+nowServing(katzDeli)
+console.log(line(katzDeli))
+takeANumber(katzDeli, "Matz")
+console.log(line(katzDeli))
+nowServing(katzDeli)
+console.log(line(katzDeli))
